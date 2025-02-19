f774362b0c41e2d67c174f586521c4c0ad9e9941
yoann <yoann.ochietti@gmail.com>
2025-02-19 14:47:46 -0500

    Implement ship placement

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,7 +1,64 @@
+import Ship from "./ship.js";
+
 export default class Gameboard {
-  placeShip(ship, position) {}
+  //2D array
+  //indicates whether each position holds a ship or not
+  boardHasShip;
+
+  //2D array
+  //indicates whether each position has been hit, missed or still untouched (null)
+  boardHits;
+
+  maxX;
+  maxY;
+
+  constructor(width, height) {
+    this.maxX = width - 1;
+    this.maxY = height - 1;
+    this.boardHasShip = [];
+    this.boardHits = [];
+
+    for (let x = 0; x < width; x++) {
+      this.boardHasShip[x] = [];
+      this.boardHits[x] = [];
+      for (let y = 0; y < height; y++) {
+        this.boardHasShip[x][y] = false;
+        this.boardHits[x][y] = false;
+      }
+    }
+  }
+
+  placeShip(position, length, isVertical) {
+    let { x: startX, y: startY } = position;
+    for (let offset = 0; offset < length; offset++) {
+      const x = isVertical ? startX : startX + offset;
+      const y = isVertical ? startY + offset : startY;
+      if (!this.#isOnBoard(x, y)) {
+        throw new Error("Ship is out of bounds");
+      }
+
+      if (this.boardHasShip[x][y]) {
+        throw new Error("Cannot place overlapping ships");
+      }
+      this.boardHasShip[x][y] = true;
+    }
+  }
 
   receiveAttack(position) {}
 
   isFullySunk() {}
+
+  containsShip(position) {}
+
+  #isOnBoard(x, y) {
+    if (x < 0 || x > this.maxX) {
+      return false;
+    }
+
+    if (y < 0 || y > this.maxY) {
+      return false;
+    }
+
+    return true;
+  }
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,10 +1,44 @@
 import Gameboard from "../gameboard.js";
 
 describe("Gameboard", () => {
+  let gb;
+
+  beforeEach(() => {
+    gb = new Gameboard(10, 10);
+  });
+
   test("api exists", () => {
     expect(Gameboard).toBeDefined();
-    expect(Gameboard.prototype.placeShip).toBeDefined();
-    expect(Gameboard.prototype.receiveAttack).toBeDefined();
-    expect(Gameboard.prototype.isFullySunk).toBeDefined();
+    expect(gb.placeShip).toBeDefined();
+    expect(gb.receiveAttack).toBeDefined();
+    expect(gb.isFullySunk).toBeDefined();
+    expect(gb.maxX).toBeDefined();
+    expect(gb.maxY).toBeDefined();
+    expect(gb.boardHits).toBeDefined();
+    expect(gb.boardHasShip).toBeDefined();
+  });
+
+  test("empty slots are indeed empty", () => {
+    expect(gb.boardHasShip[0][0]).toBeFalsy();
+  });
+
+  test("placement happy path", () => {
+    gb.placeShip({ x: 0, y: 0 }, 10, true);
+    expect(gb.boardHasShip[0][0]).toBeTruthy();
+    expect(gb.boardHasShip[0][1]).toBeTruthy();
+    expect(gb.boardHasShip[0][2]).toBeTruthy();
+  });
+
+  test("placement outside bounds throws", () => {
+    expect(() => gb.placeShip({ x: 11, y: 0 }, 1, true)).toThrow("bounds");
+  });
+
+  test("placing ship that wont fit throws", () => {
+    expect(() => gb.placeShip({ x: 0, y: 0 }, 11, true)).toThrow("bounds");
+  });
+
+  test("placing overlapping ships throws", () => {
+    gb.placeShip({ x: 0, y: 0 }, 2, false);
+    expect(() => gb.placeShip({ x: 0, y: 0 }, 2, true)).toThrow("overlap");
   });
 });
