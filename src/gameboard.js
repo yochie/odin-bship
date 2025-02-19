9c5dd57f5b50b33b33e984c40898fd29c6a4418f
yoann <yoann.ochietti@gmail.com>
2025-02-19 15:59:05 -0500

    Implement receiveAttack
    Includes bug fix for aborted ship placements that were leaving behind
    some board state. Including here because fix applies to both features.

--- src/gameboard.js
+++ src/gameboard.js
@@ -9,6 +9,12 @@ export default class Gameboard {
   //indicates whether each position has been hit, missed or still untouched (null)
   boardHits;
 
+  //2D array pointing to ships at each position
+  #boardShips;
+
+  //list of ships on board without positions
+  #ships;
+
   maxX;
   maxY;
 
@@ -17,19 +23,25 @@ export default class Gameboard {
     this.maxY = height - 1;
     this.boardHasShip = [];
     this.boardHits = [];
+    this.#boardShips = [];
+    this.#ships = [];
 
     for (let x = 0; x < width; x++) {
       this.boardHasShip[x] = [];
       this.boardHits[x] = [];
+      this.#boardShips[x] = [];
       for (let y = 0; y < height; y++) {
         this.boardHasShip[x][y] = false;
-        this.boardHits[x][y] = false;
+        this.boardHits[x][y] = null;
+        this.#boardShips[x][y] = null;
       }
     }
   }
 
   placeShip(position, length, isVertical) {
     let { x: startX, y: startY } = position;
+
+    //do a first pass over all occupied positions to ensure they are valid
     for (let offset = 0; offset < length; offset++) {
       const x = isVertical ? startX : startX + offset;
       const y = isVertical ? startY + offset : startY;
@@ -40,15 +52,43 @@ export default class Gameboard {
       if (this.boardHasShip[x][y]) {
         throw new Error("Cannot place overlapping ships");
       }
+    }
+
+    //do a second pass that actually modifies state now that we know
+    //modification is valid
+    const ship = new Ship(length);
+    for (let offset = 0; offset < length; offset++) {
+      const x = isVertical ? startX : startX + offset;
+      const y = isVertical ? startY + offset : startY;
       this.boardHasShip[x][y] = true;
+      this.#boardShips[x][y] = ship;
+    }
+
+    this.#ships.push(ship);
+  }
+
+  receiveAttack(position) {
+    const { x, y } = position;
+    if (this.boardHits[x][y] !== null) {
+      throw new Error("Cannot attack same position twice");
+    }
+
+    if (this.boardHasShip[x][y]) {
+      this.boardHits[x][y] = "hit";
+      this.#boardShips[x][y].hit();
+    } else {
+      this.boardHits[x][y] = "miss";
     }
   }
 
-  receiveAttack(position) {}
-
-  isFullySunk() {}
-
-  containsShip(position) {}
+  isFullySunk() {
+    for (let ship of this.#ships) {
+      if (!ship.isSunk()) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   #isOnBoard(x, y) {
     if (x < 0 || x > this.maxX) {
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -37,8 +37,70 @@ describe("Gameboard", () => {
     expect(() => gb.placeShip({ x: 0, y: 0 }, 11, true)).toThrow("bounds");
   });
 
+  test("aborted ship placement clears hasShip state", () => {
+    expect(() => gb.placeShip({ x: 0, y: 0 }, 11, true)).toThrow("bounds");
+    expect(gb.boardHasShip[0][0]).toBeFalsy();
+  });
+
   test("placing overlapping ships throws", () => {
     gb.placeShip({ x: 0, y: 0 }, 2, false);
     expect(() => gb.placeShip({ x: 0, y: 0 }, 2, true)).toThrow("overlap");
+  });
+
+  test("unattacked positions have null boardhit state", () => {
+    expect(gb.boardHits[0][0]).toBeNull();
+  });
+
+  test("missed attacks are recorded", () => {
+    gb.receiveAttack({ x: 0, y: 0 });
+    expect(gb.boardHits[0][0]).toBe("miss");
+  });
+
+  //note we do not test that ship is actually hit
+  //I consider this is an internal side effect
+  //this will be tested indirectly when ensuring all shits are sunk
+  test("landing attacks are recorded", () => {
+    gb.placeShip({ x: 0, y: 0 }, 2, false);
+    gb.receiveAttack({ x: 0, y: 0 });
+    expect(gb.boardHits[0][0]).toBe("hit");
+  });
+
+  test("cannot attack same position twice", () => {
+    const position = { x: 0, y: 0 };
+    gb.receiveAttack(position);
+    expect(() => gb.receiveAttack(position)).toThrow("same");
+  });
+
+  test("attacking can sink single ship", () => {
+    gb.placeShip({ x: 0, y: 0 }, 2, false);
+    gb.receiveAttack({ x: 0, y: 0 });
+    gb.receiveAttack({ x: 1, y: 0 });
+    expect(gb.isFullySunk()).toBeTruthy();
+  });
+
+  test("attacking can sink multiple ships", () => {
+    gb.placeShip({ x: 0, y: 0 }, 2, false);
+    gb.receiveAttack({ x: 0, y: 0 });
+    gb.receiveAttack({ x: 1, y: 0 });
+    gb.placeShip({ x: 0, y: 1 }, 2, false);
+    gb.receiveAttack({ x: 0, y: 1 });
+    gb.receiveAttack({ x: 1, y: 1 });
+    expect(gb.isFullySunk()).toBeTruthy();
+  });
+
+  test("sinking single single ship is not fully sunk", () => {
+    gb.placeShip({ x: 0, y: 0 }, 2, false);
+    gb.placeShip({ x: 0, y: 1 }, 2, false);
+    gb.receiveAttack({ x: 0, y: 0 });
+    gb.receiveAttack({ x: 1, y: 0 });
+    expect(gb.isFullySunk()).toBeFalsy();
+  });
+
+  test("aborted ship placements dont count for board sunk status", () => {
+    expect(() => gb.placeShip({ x: 0, y: 0 }, 11, false)).toThrow("bound");
+    gb.placeShip({ x: 0, y: 0 }, 2, false);
+    gb.receiveAttack({ x: 0, y: 0 });
+    gb.receiveAttack({ x: 1, y: 0 });
+    expect(gb.isFullySunk()).toBeTruthy();
   });
 });
